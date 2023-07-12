5dba14258a4b1b15d6e313c8033c29c38fb5439e
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-07-12 15:09:25 +0200

    probemos

--- src/screens/camera.tsx
+++ src/screens/camera.tsx
@@ -3,7 +3,10 @@ import { Camera, useCameraDevices } from "react-native-vision-camera";
 import useIsForeground from "../hooks/IsAppForeground.hook";
 import { useIsFocused } from "@react-navigation/native";
 import { View } from "react-native";
+useScanBarcodes
 import 'react-native-reanimated'
+import { useScanBarcodes , BarcodeFormat } from "vision-camera-code-scanner";
+import { useEffect } from "react";
 
 const CameraOverlay = ({children}:{children:JSX.Element}) => {
 
@@ -12,14 +15,20 @@ const CameraOverlay = ({children}:{children:JSX.Element}) => {
     const IsAppForeground = useIsForeground()
     const isFocused = useIsFocused()
 
+    const [ frameProcessor , barcodes ] = useScanBarcodes([BarcodeFormat.QR_CODE], { checkInverted: true });
+
+    useEffect(() => console.log(barcodes),[barcodes]);
+
     return(
         (device == null)
         ?   <Text>CARGANDO CAMARA...</Text>
         :   <View style={[StyleSheet.absoluteFill]}>
                 <Camera
-                style={[StyleSheet.absoluteFill]}
-                device={device}
-                isActive={(IsAppForeground && isFocused)}
+                    style={[StyleSheet.absoluteFill]}
+                    device={device}
+                    isActive={(IsAppForeground && isFocused)}
+                    frameProcessor={frameProcessor}
+                    frameProcessorFps={3}
                 />
                 {children}
             </View>
@@ -31,10 +40,15 @@ const CameraComponent = () => {
 
     return(
         <CameraOverlay>
-            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}><Text>LOREM IPSUM DOLOR</Text></View>
+            <View style={styles.container}><Text>LOREM IPSUM DOLOR</Text></View>
         </CameraOverlay>
     )
 
 }
+
+const styles = StyleSheet.create({
+    container:{flex:1,justifyContent:'center',alignItems:'center'},
+    barcodeTextURL: {fontSize: 20,color: 'white',fontWeight: 'bold'},
+})
 
 export default CameraComponent
