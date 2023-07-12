00ad4e912fc4a5ce63ad44ab094b30cc566aec6f
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-07-12 13:15:15 +0200

    tenemos que instalar el react native reanimated

--- src/screens/camera.tsx
+++ src/screens/camera.tsx
@@ -2,26 +2,36 @@ import { StyleSheet, Text } from "react-native";
 import { Camera, useCameraDevices } from "react-native-vision-camera";
 import useIsForeground from "../hooks/IsAppForeground.hook";
 import { useIsFocused } from "@react-navigation/native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { View } from "react-native";
 
-const CameraComponent = () => {
+const CameraOverlay = ({children}:{children:JSX.Element}) => {
 
     const devices = useCameraDevices();
     const device = devices.back;
     const IsAppForeground = useIsForeground()
     const isFocused = useIsFocused()
-  
-    if (device == null) return <Text>CARGANDO CAMARA...</Text>
-    return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Camera
-        style={[StyleSheet.absoluteFill]}
-        device={device}
-        isActive={(IsAppForeground && isFocused)}
-      />
-      <Text>Lorem ipsum DOLOR</Text>
-    </View>
+
+    return(
+        (device == null)
+        ?   <Text>CARGANDO CAMARA...</Text>
+        :   <View style={[StyleSheet.absoluteFill]}>
+                <Camera
+                style={[StyleSheet.absoluteFill]}
+                device={device}
+                isActive={(IsAppForeground && isFocused)}
+                />
+                {children}
+            </View>
+    )
+
+}
+
+const CameraComponent = () => {
+
+    return(
+        <CameraOverlay>
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}><Text>LOREM IPSUM DOLOR</Text></View>
+        </CameraOverlay>
     )
 
 }
