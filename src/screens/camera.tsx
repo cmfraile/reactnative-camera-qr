2e2ada8d1f5282334fe952c851c78ad3b71b189a
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-07-12 13:00:59 +0200

    overlay

--- src/screens/camera.tsx
+++ src/screens/camera.tsx
@@ -1,20 +1,29 @@
 import { StyleSheet, Text } from "react-native";
 import { Camera, useCameraDevices } from "react-native-vision-camera";
-StyleSheet
+import useIsForeground from "../hooks/IsAppForeground.hook";
+import { useIsFocused } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 const CameraComponent = () => {
-    const devices = useCameraDevices()
-    const device = devices.back
+
+    const devices = useCameraDevices();
+    const device = devices.back;
+    const IsAppForeground = useIsForeground()
+    const isFocused = useIsFocused()
   
     if (device == null) return <Text>CARGANDO CAMARA...</Text>
     return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <Camera
         style={[StyleSheet.absoluteFill]}
         device={device}
-        isActive={true}
+        isActive={(IsAppForeground && isFocused)}
       />
+      <Text>Lorem ipsum DOLOR</Text>
+    </View>
     )
-    
+
 }
 
 export default CameraComponent
